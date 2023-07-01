3ad17deff1671c1f837f9492ed389da440b41e5f
Sullivan Myer <slucasmyer@icloud.com>
2023-07-01 17:02:51 -0400

    dev

--- components/TriangleIcon.tsx
+++ components/TriangleIcon.tsx
@@ -1,5 +1,6 @@
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 export default function TriangleIcon(props: SvgIconProps) {
+    console.log(props);
     return (
         <SvgIcon {...props}>
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1"  viewBox="0 0 800 800">
